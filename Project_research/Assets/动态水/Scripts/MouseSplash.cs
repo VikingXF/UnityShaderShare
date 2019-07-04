c4225a751efc3cf3bdd0d1824cbe586d59436158
xuefei <554524329@qq.com>
2019-07-04 14:15:43 +0800

    注销

--- Project_research/Assets/动态水/Scripts/MouseSplash.cs
+++ Project_research/Assets/动态水/Scripts/MouseSplash.cs
@@ -4,7 +4,7 @@
 public class MouseSplash : MonoBehaviour
 {
 
-    public DynamicWater Water = null;
+    //public DynamicWater Water = null;
     public float SplashForce = 10f;
     public float SplashRadius = 0.25f;
     public Camera Camera;
@@ -15,10 +15,10 @@ public class MouseSplash : MonoBehaviour
     // Updating the splash generation
     private void FixedUpdate()
     {
-        if (Water == null)
-        {
-            return;
-        }
+        //if (Water == null)
+       // {
+       //     return;
+        //}
 
         if (Camera == null)
         {
@@ -56,17 +56,17 @@ public class MouseSplash : MonoBehaviour
         }
 
         // Checking for collision
-        Physics.Raycast(ray, out hitInfo, Mathf.Infinity,
-                        1 << LayerMask.NameToLayer(DynamicWater.PlaneColliderLayerName));
+       // Physics.Raycast(ray, out hitInfo, Mathf.Infinity,
+       //                 1 << LayerMask.NameToLayer(DynamicWater.PlaneColliderLayerName));
 
         // Creating a splash line between previous position and current
         if (GUIUtility.hotControl == 0 && (Input.GetMouseButton(0) || Input.touchCount > 0))
         {
-            if (hitInfo.transform != null && Water != null && prevPoint != Vector3.zero)
-            {
-                Water.GetWaterLevel(hitInfo.point.x, hitInfo.point.y, hitInfo.point.z);
-                Water.CreateSplash(prevPoint, hitInfo.point, SplashRadius, -SplashForce * Time.deltaTime);
-            }
+            //if (hitInfo.transform != null && Water != null && prevPoint != Vector3.zero)
+            //{
+            //    Water.GetWaterLevel(hitInfo.point.x, hitInfo.point.y, hitInfo.point.z);
+            //    Water.CreateSplash(prevPoint, hitInfo.point, SplashRadius, -SplashForce * Time.deltaTime);
+           // }
         }
 
         prevPoint = hitInfo.transform != null ? hitInfo.point : Vector3.zero;
