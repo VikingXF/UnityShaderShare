5cc68018a259d23a4af16eaa66a2281829d8e09b
xuefei <554524329@qq.com>
2019-11-15 09:16:58 +0800

    特效批量操作

---
+++ Project_research/Assets/Tools/ParticleBatch/SetParticleLayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+
+#endif
+
+[ExecuteInEditMode]
+public class SetParticleLayer : MonoBehaviour
+{
+    public int particleLayer = 0;
+    float prevLayer;
+
+    void Start()
+    {
+        prevLayer = particleLayer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (prevLayer != particleLayer)
+        {
+
+            LayerRenderers();
+
+        }
+    }
+
+    void LayerRenderers()
+    {
+#if UNITY_EDITOR
+        ParticleSystemRenderer[] PSystemsRenders = GetComponentsInChildren<ParticleSystemRenderer>();
+
+        foreach (ParticleSystemRenderer PSystemsRender in PSystemsRenders)
+        {
+            PSystemsRender.sortingOrder = particleLayer;
+
+        }
+
+#endif
+    }
+
+}
