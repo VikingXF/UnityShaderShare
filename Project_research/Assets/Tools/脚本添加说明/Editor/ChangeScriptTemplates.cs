8062455ef62299e4caff78667a4746805f02278b
xuefei <554524329@qq.com>
2020-04-14 17:38:53 +0800

    脚本说明脚本

---
+++ Project_research/Assets/Tools/脚本添加说明/Editor/ChangeScriptTemplates.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+public class ChangeScriptTemplates : UnityEditor.AssetModificationProcessor
+{
+    private static string str =
+          "//=============================================\r\n"
+        + "//作者:\r\n"
+        + "//描述:\r\n"
+        + "//创建时间:#CreateTime#\r\n"
+        + "//版本:v 1.0\r\n"
+        + "//=============================================\r\n";
+
+    public static void OnWillCreateAsset(string path)
+    {
+        path = path.Replace(".meta", "");
+        if (path.EndsWith(".cs"))
+        {
+            string allText = str;
+            allText += File.ReadAllText(path);
+            allText = allText.Replace("#CreateTime#", System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"));
+            File.WriteAllText(path, allText);
+        
+        }
+    }
+
+}
