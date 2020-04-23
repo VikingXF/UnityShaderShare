a7c14e499f8107fe0d15d73375a852700a8dde35
xuefei <554524329@qq.com>
2020-04-23 11:51:40 +0800

    抗锯齿

---
+++ Project_research/Assets/Tools/AntiAliasing/Editor/IAntiAliasingEditor.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+namespace UnityStandardAssets.CinematicEffects
+{
+    public interface IAntiAliasingEditor
+    {
+        void OnEnable(SerializedObject serializedObject, string path);
+        bool OnInspectorGUI(IAntiAliasing target);
+    }
+}
