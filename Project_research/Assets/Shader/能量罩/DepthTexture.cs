24c9405e4d50fc2088d32f41aa10eee8d0e80be4
Ѧ·É <xuefei@babybus.com>
2020-07-09 10:51:14 +0800

    能量罩

---
+++ Project_research/Assets/Shader/能量罩/DepthTexture.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DepthTexture : MonoBehaviour
+{
+
+
+    private Camera myCamera;
+    public Camera camera
+    {
+        get
+        {
+            if (myCamera == null)
+            {
+                myCamera = GetComponent<Camera>();
+            }
+            return myCamera;
+        }
+    }
+
+    void OnEnable()
+    {
+        camera.depthTextureMode |= DepthTextureMode.Depth;
+    }
+
+}
