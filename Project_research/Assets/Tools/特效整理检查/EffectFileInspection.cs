18ae4759c9ff968f1065876ef0db1173317c753d
薛飞 <xuefei@babybus.com>
2020-09-02 15:23:53 +0800

    添加比较shader功能

--- Project_research/Assets/Tools/特效整理检查/EffectFileInspection.cs
+++ Project_research/Assets/Tools/特效整理检查/EffectFileInspection.cs
@@ -28,6 +28,9 @@ namespace OptimizationTools
         Vector2 scrollPosition3;
         Vector2 scrollPosition4;
         Vector2 scrollPosition5;
+        Vector2 scrollPosition6;
+        Vector2 scrollPosition7;
+        Vector2 scrollPosition8;
         //shader列表
         #region 
         [SerializeField]//必须要加  
@@ -35,6 +38,12 @@ namespace OptimizationTools
 
         [SerializeField]//必须要加  
         public List<Shader> _ChangeshaderAssetLst = new List<Shader>();
+
+        [SerializeField]//必须要加  
+        public List<Shader> _OldshaderAssetLst = new List<Shader>();
+
+        [SerializeField]//必须要加  
+        public List<Shader> _ResultshaderAssetLst = new List<Shader>();
 
         [SerializeField]
         public List<Material> _EffectMaterialAssetLst = new List<Material>();
@@ -48,6 +57,16 @@ namespace OptimizationTools
         protected SerializedObject _serializedChangeShader;
         //序列化属性  
         protected SerializedProperty _ChangeShaderAssetLstProperty;
+
+        //序列化对象  
+        protected SerializedObject _serializedOldShader;
+        //序列化属性  
+        protected SerializedProperty _OldShaderAssetLstProperty;
+        
+        //序列化对象  
+        protected SerializedObject _serializedResultShader;
+        //序列化属性  
+        protected SerializedProperty _ResultShaderAssetLstProperty;
 
         //序列化对象  
         protected SerializedObject _serializedMaterial;
@@ -129,6 +148,17 @@ namespace OptimizationTools
             //获取当前类中可序列话的属性
             _ChangeShaderAssetLstProperty = _serializedChangeShader.FindProperty("_ChangeshaderAssetLst");
 
+            //使用当前类初始化
+            _serializedOldShader = new SerializedObject(this);
+            //获取当前类中可序列话的属性
+            _OldShaderAssetLstProperty = _serializedOldShader.FindProperty("_OldshaderAssetLst");
+
+            //使用当前类初始化
+            _serializedResultShader = new SerializedObject(this);
+            //获取当前类中可序列话的属性
+            _ResultShaderAssetLstProperty = _serializedResultShader.FindProperty("_ResultshaderAssetLst");
+
+            
 
             //使用当前类初始化
             _serializedMaterial = new SerializedObject(this);
@@ -180,22 +210,22 @@ namespace OptimizationTools
             GUILayout.BeginVertical("box", GUILayout.Width(420));
             GUILayout.Label("相同名字shader切换");
 
-            if (GUILayout.Button("相同名字shader切换", GUILayout.Width(400), GUILayout.Height(30)))
+            if (GUILayout.Button("相同名字shader切换", GUILayout.Height(30)))
             {
 
                 Changeshader();
             }
 
-            GUILayout.EndVertical();
+           // GUILayout.EndVertical();
             //===============================
-            GUILayout.BeginVertical("box", GUILayout.Width(420));
-            GUILayout.Label("shader列表");
+           // GUILayout.BeginVertical("box", GUILayout.Width(420));
+            GUILayout.Label("修改后shader列表");
             //更新  
             _serializedChangeShader.Update();
 
             //开始检查是否有修改  
             EditorGUI.BeginChangeCheck();
-            scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+            scrollPosition6 = GUILayout.BeginScrollView(scrollPosition6);
             //显示属性  
             //第二个参数必须为true，否则无法显示子节点即List内容  
             EditorGUILayout.PropertyField(_ChangeShaderAssetLstProperty, true);
@@ -207,15 +237,75 @@ namespace OptimizationTools
             }
 
             GUILayout.EndVertical();
-            //===================================================
+
             
+    
+            GUILayout.BeginVertical("box", GUILayout.Width(420));
+            GUILayout.Label("需要修改的shader列表");
+            //更新  
+            _serializedOldShader.Update();
+
+            //开始检查是否有修改  
+            EditorGUI.BeginChangeCheck();
+            scrollPosition7 = GUILayout.BeginScrollView(scrollPosition7);
+            //显示属性  
+            //第二个参数必须为true，否则无法显示子节点即List内容  
+            EditorGUILayout.PropertyField(_OldShaderAssetLstProperty, true);
+            GUILayout.EndScrollView();
+            //结束检查是否有修改  
+            if (EditorGUI.EndChangeCheck())
+            {
+                _serializedOldShader.ApplyModifiedProperties();
+            }
+            GUILayout.EndVertical();
+            //===================================================
+            GUILayout.BeginVertical("box", GUILayout.Width(420));
+            GUILayout.Label("shader比较");
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("shader比较", GUILayout.Height(30)))
+            {
+                CompareShader();
+            }
+            //if (GUILayout.Button("一键移动到对应文件夹", GUILayout.Height(30)))
+            //{
+
+            //}
+            GUILayout.EndHorizontal();
+            GUILayout.EndVertical();
+
+            //=============
+            GUILayout.BeginVertical("box", GUILayout.Width(420));
+            GUILayout.Label("比较结果shader列表");
+            //更新  
+            _serializedResultShader.Update();
+
+            //开始检查是否有修改  
+            EditorGUI.BeginChangeCheck();
+            scrollPosition8 = GUILayout.BeginScrollView(scrollPosition8);
+            //显示属性  
+            //第二个参数必须为true，否则无法显示子节点即List内容  
+            EditorGUILayout.PropertyField(_ResultShaderAssetLstProperty, true);
+            GUILayout.EndScrollView();
+            //结束检查是否有修改  
+            if (EditorGUI.EndChangeCheck())
+            {
+                _serializedResultShader.ApplyModifiedProperties();
+            }
+            GUILayout.EndVertical();
+
             //===============================
             GUILayout.BeginVertical("box", GUILayout.Width(420));
-            if (GUILayout.Button("查找引用列表中shader的材质", GUILayout.Width(400), GUILayout.Height(30)))
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("查找需要修改的shader列表中的材质",  GUILayout.Height(30)))
             {
 
                 shaderFindMaterial();
             }
+            if (GUILayout.Button("对下列材质列表进行相同名字shader切换", GUILayout.Height(30)))
+            {
+                ResultChangeshader();
+            }
+            GUILayout.EndHorizontal();
             GUILayout.Label("材质列表");
             //更新  
             _serializedMaterial.Update();
@@ -236,7 +326,26 @@ namespace OptimizationTools
             GUILayout.EndVertical();
             //===================================================
         }
-
+        private void CompareShader()
+        {
+            List<Shader> DifferentShader = new List<Shader>();
+            if (_ChangeshaderAssetLst.Count == 0 || _OldshaderAssetLst.Count ==0)
+            {
+                EditorUtility.DisplayDialog("警告", "列表中shader是空的！", "OK");
+                return;
+            }
+            foreach(Shader shader in _OldshaderAssetLst)
+            {
+                for (int i = 0; i < _ChangeshaderAssetLst.Count; i++)
+                {
+                    if (shader != _ChangeshaderAssetLst[i])
+                    {
+                        DifferentShader.Add(shader);
+                    }
+                }
+            }
+            _ResultshaderAssetLst = DifferentShader;
+        }
         private void shaderFindMaterial()
         {
             Object[] Materilas = GetSelectedMaterilas();
@@ -248,9 +357,9 @@ namespace OptimizationTools
             List<Material> ErrShaderMaterial = new List<Material>();
             foreach (Material material in Materilas)
             {
-                for (int i = 0; i < _ChangeshaderAssetLst.Count; i++)
+                for (int i = 0; i < _OldshaderAssetLst.Count; i++)
                 {
-                    if (material.shader == _ChangeshaderAssetLst[i])
+                    if (material.shader == _OldshaderAssetLst[i])
                     {
                         ErrShaderMaterial.Add(material);
 
@@ -289,7 +398,34 @@ namespace OptimizationTools
                 
             }
         }
+        private void ResultChangeshader()
+        {
+         
+            if (_EffectMaterialAssetLst.Count == 0)
+            {
+                EditorUtility.DisplayDialog("警告", "材质列表是空的！", "OK");
+                return;
+            }
+            foreach (Material material in _EffectMaterialAssetLst)
+            {
 
+                for (int i = 0; i < _ChangeshaderAssetLst.Count; i++)
+                {
+                    if (material.shader.name == _ChangeshaderAssetLst[i].name)
+                    {
+                        if (material.shader != _ChangeshaderAssetLst[i])
+                        {
+                            int RenderQueueint = material.renderQueue;
+                            material.shader = _ChangeshaderAssetLst[i];
+                            material.renderQueue = RenderQueueint;
+                        }
+
+                    }
+
+                }
+
+            }
+        }
 
         #endregion
 
